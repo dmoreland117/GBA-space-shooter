6edaaf951664ed37f3ea28f26c892b99d5f6af0e
dmoreland117 <61122492+dmoreland117@users.noreply.github.com>
2026-04-15 16:03:41 -0400

    removed static from states and entity table define

--- game/include/memory_sections.h
+++ game/include/memory_sections.h
@@ -4,7 +4,7 @@
 #include "engine/entity/entity.h"
 #include "engine/state/state.h"
 
-#define ENTITIES_TABLE __attribute__((section(".entities_table"))) static const
+#define ENTITIES_TABLE __attribute__((section(".entities_table"))) const
 
 extern const EntityData __entities_table_start;
 extern const EntityData __entities_table_end;
@@ -12,7 +12,7 @@ extern const EntityData __entities_table_end;
 #define ENTITIES_TABLE_START &__entities_table_start
 #define ENTITIES_TABLE_END   &__entities_table_end
 
-#define STATES_TABLE __attribute__((section(".states_table"))) static const
+#define STATES_TABLE __attribute__((section(".states_table"))) const
 
 extern const State __states_table_start;
 extern const State __states_table_end;
